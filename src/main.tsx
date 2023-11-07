4100bf3fc44240f58d3ba4f9ff1bcdc496023cd8
Andrew Blakey <ablakey@locusrobotics.com>
2023-11-07 17:29:28 -0500

    update

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { Microwave } from "./Microwave";
-
-const container = document.getElementById("frame");
-const root = createRoot(container!);
-
-root.render(<Microwave />);
